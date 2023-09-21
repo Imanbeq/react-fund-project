e716d1e25a72d7aeb17c6fa80777627a87881af3
Qlove Iman <imanqlove@gamil.com>
2023-09-21 13:20:58 +0600

    added useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 import Employee from './components/Employee';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div>
       {showEmployees ? (
         <>
+          <input
+            type='text'
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
           <Employee name='Caleb' role='intern' />
           <Employee name='Adam' role='security' />
-          <Employee name='Susan' role='nurse' />
+          <Employee name='Susan' role={role} />
           <Employee name='Becky' />
         </>
       ) : (
