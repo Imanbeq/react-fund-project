7a0bc32e9f38b5b05d92db216628bdb525136757
Qlove Iman <imanqlove@gamil.com>
2023-10-02 15:24:51 +0600

    added AddCustomer.js and newCustomer function

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared';
+import AddCustomer from '../components/AddCustomer';
 
 const Customers = () => {
   const [customers, setCustomers] = useState();
@@ -13,22 +14,48 @@ const Customers = () => {
         setCustomers(data.customers);
       });
   }, []);
+
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + 'api/customers/';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Sometinhg went wrong!');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // assume the add was succesful
+        // hide the modal
+        // make sure the list is updated appropriately
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <>
       <h1>Hello are our customers: </h1>
       <ul>
-      {customers
-        ? customers.map((customer) => {
-            return (
-              
+        {customers
+          ? customers.map((customer) => {
+              return (
                 <li key={customer.id}>
                   <Link to={'/customers/' + customer.id}>{customer.name}</Link>
                 </li>
-              
-            );
-          })
-        : null}
-        </ul>
+              );
+            })
+          : null}
+      </ul>
+      <AddCustomer newCustomer={newCustomer} />
     </>
   );
 };
